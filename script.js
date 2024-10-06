79a352be6aa7a0eef612295b128aa1f802d0d121
Aditya Gupta <148680267+adi9336@users.noreply.github.com>
2024-10-07 02:54:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var elem = document.querySelectorAll(".elem");
+
+elem.forEach(function(val){
+    val.addEventListener("mousemove",function(dets){
+        val.childNodes[3].style.left = dets.x+"px"
+        val.childNodes[3].style.top = dets.y+"px"
+        // val.childNodes[3].style.scale = "2"
+    })
+
+    val.addEventListener("mouseenter",function(){
+        val.childNodes[3].style.opacity = "1";
+    })
+    val.addEventListener("mouseleave",function(){
+        val.childNodes[3].style.opacity = "0";
+    })
+})
